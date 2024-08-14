bb6494081ee3190ef5f247fbdcda047af4137776
Ethan Hubbartt <33075924+ehubbartt@users.noreply.github.com>
2024-08-14 01:17:55 -0400

    change gpio slowdown

--- matrix/bindings/python/samples/firebase.py
+++ matrix/bindings/python/samples/firebase.py
@@ -17,6 +17,7 @@ options.cols = 64
 options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = 'adafruit-hat'  # Ensures correct hardware mapping for the bonnet
+options.gpio_slowdown = 4
 
 matrix = RGBMatrix(options=options)
 
